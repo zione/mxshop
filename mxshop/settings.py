ae9a5524f656e7ff6f06ca91f94dc98fe3f68f8a
zione <huangmayi@126.com>
2018-06-04 17:15:57 +0800

    默认的

--- mxshop/settings.py
+++ mxshop/settings.py
@@ -149,7 +149,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "cstatic"),
+STATIC_ROOT = os.path.join(BASE_DIR, "cstatic")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
